1483f934a04e483c94ac6fdfd15025aa0439b0a7
AbdSh17 <154285986+AbdSh17@users.noreply.github.com>
2024-05-08 20:44:38 +0300

    Delete auto increse ( handeled by java )

--- Music/MySql/Clinic.sql
+++ Music/MySql/Clinic.sql
@@ -4,32 +4,37 @@ use Clinic;
 
 -- /-----------------------------------------------------------------------------------------------------
 
-drop table if exists Patients; 
+drop table if exists Patients;
 create table Patients(
-P_name varchar(32),
-P_id int AUTO_INCREMENT,
-p_phonenumber varchar(12) ,
+P_name varchar(32) unique not null,
+P_id int,
+p_phonenumber varchar(12) not null,
+p_bdate date not null,
+p_age int , -- drived attribute 
 primary key (P_id)
 );
 
+
+select * from patients ;
 -- /------------------------------------------------------------------------------------
 drop table if exists Treatmenthistory; 
-create table Treatmenthistory(
+create table Treatmenthistory (
 Ttype varchar(32),
 Ttime time,
 P_id int,
 Tdate date,
 Tname  varchar(32),
-primary key (Tname,P_id),
+primary key (Tname, P_id),
 foreign key (P_id) references Patients(P_id) on delete cascade
 );
 
 -- /-------------------------------------------------------------------------------------------------------------------
 drop table if exists Diagnosis; 
 create table Diagnosis(
-Did int AUTO_INCREMENT,
-Dtype varchar(32),
-primary key(Did));
+Did int,
+Dtype varchar(32) not null,
+primary key(Did)
+);
 
 
 -- /--------------------------------------------------------------------
@@ -37,7 +42,7 @@ drop table if exists Diagnosis_patient;
 create table Diagnosis_patient(
 Did int,
 P_id int,
-primary key(Did,P_id),
+primary key(Did, P_id),
 foreign key (P_id) references Patients(P_id),
 foreign key (Did) references Diagnosis(Did)
 );
@@ -45,7 +50,7 @@ foreign key (Did) references Diagnosis(Did)
 
 drop table if exists Appointments; 
 create table Appointments(
-Appointment_id int AUTO_INCREMENT,
+Appointment_id int,
 A_Date date,
 A_time time,
 P_id int,
@@ -60,7 +65,7 @@ foreign key (R_id) references Room(R_id)
 -- /---------------------------------------------------------------------
 drop table if exists Finantial; 
 create table Finantial(
-F_id int AUTO_INCREMENT,
+F_id int ,
 amount int ,
 balance  int,
 F_type varchar(32),
@@ -72,7 +77,7 @@ foreign key(P_id) references Patients(P_id)
 -- /-----------------------------------------------
 drop table if exists Room; 
 create table Room(
-R_id int AUTO_INCREMENT,
+R_id int ,
 service_type varchar(32),
 d_id int,
 primary key(R_id),
@@ -83,10 +88,10 @@ foreign key(d_id) references doctor(d_id)
 
 drop table if exists Doctor; 
 create table  Doctor(
-D_id int AUTO_INCREMENT,
-D_name varchar(32),
+D_id int,
+D_name varchar(32) not null,
 D_speciality varchar(32),
-D_phonenumber varchar(12),
+D_phonenumber varchar(12), -- 05..
 C_id int,
 foreign key (C_id) references Clinic(C_id),
 primary key(D_id)
@@ -95,13 +100,15 @@ primary key(D_id)
 -- /----------------------------------------------------
 drop table if exists Clinic; 
 create table  Clinic(
-C_id int AUTO_INCREMENT,
-C_name varchar(32),
-C_phonenumber Long,
-D_id int,
-primary key(C_id),
-foreign key(d_id) references doctor(d_id)
+C_id int,
+C_name varchar(32) not null,
+C_phonenumber varchar(12) ,
+primary key(C_id)
 );
+
+alter table clinic
+add column d_id int,
+ADD CONSTRAINT FOREIGN KEY (d_id) REFERENCES doctor(d_id);
 
 
 -- /------------------------------------------------
@@ -109,7 +116,7 @@ drop table if exists Laboratory;
 create table  Laboratory(
 L_id int,
 C_id int,
-L_name varchar(32) AUTO_INCREMENT,
+L_name varchar(32) not null,
 L_phonenumber varchar(12) ,
 L_service varchar(32),
 foreign key (C_id) references Clinic(C_id),
@@ -119,7 +126,7 @@ primary key(L_id)
 -- /-----------------------------------------------
 drop table if exists Payment ; 
 create table  Payment(
-P_id int AUTO_INCREMENT,
+P_id int,
 L_id int,
 P_amount_owns varchar(32),
 P_amount_mustpay varchar(32),
